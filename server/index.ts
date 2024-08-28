0680282f9a692d413f2facb0d24fe8703a3eb656
DeLesslin George-Warren <delesslingw@gmail.com>
2024-08-28 15:27:40 -0400

    adding to faux db

--- server/index.ts
+++ server/index.ts
@@ -2,7 +2,7 @@ const express = require("express")
 
 const app = express()
 const PORT = process.env.PORT || 5000
-const items = [1, 2, 3, 4, 5]
+const items = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 app.use(express.static("build"))
 app.get('/api/items', (req, res) => {
     res.send(items)
